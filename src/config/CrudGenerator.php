bfcfd93c874755f2e39810af96140e15980cf51e
Ahmed Hussain <113634512+Ahmedhussain70@users.noreply.github.com>
2025-04-22 15:02:42 +0200

    upd

--- src/config/CrudGenerator.php
+++ src/config/CrudGenerator.php
@@ -50,7 +50,7 @@ return [
     ],
 
     'providors' => [
-        ahmed-hussain70\crud-generator\CrudGeneratorServiceProvider::class,
+        AhmedHussain70\CrudGenerator\CrudGeneratorServiceProvider::class,
     ]
 
 ];
