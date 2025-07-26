fb24a01f7e530b324a071261dcd7daeaf6982fa8
Ahmed Hussain <113634512+Ahmedhussain70@users.noreply.github.com>
2025-07-26 17:22:25 +0300

    handle model name and router group

--- src/Commands/MakeRepoCrud.php
+++ src/Commands/MakeRepoCrud.php
@@ -123,13 +123,13 @@ class MakeRepoCrud extends Command
         $file = base_path('routes/api.php');
         // $routeEntry = "Route::resource('$routeName', \App\Http\Controllers\{$routeName}Controller::class);";
         $routeEntry = "
-                Route::prefix('$routeName')->group(function () {
-                Route::any('get', [$routeName::class, 'all']);
-                Route::any('add', [$routeName::class, 'store']);
-                Route::any('upd/{id}', [$routeName::class, 'update']);
-                Route::any('del/{id}', [$routeName::class, 'delete']);
-                Route::any('deps', [$routeName::class, 'deps']);
-            });
+Route::prefix('$routeName')->group(function () {
+    Route::any('get', [$routeName::class, 'all']);
+    Route::any('add', [$routeName::class, 'store']);
+    Route::any('upd/{id}', [$routeName::class, 'update']);
+    Route::any('del/{id}', [$routeName::class, 'delete']);
+    Route::any('deps', [$routeName::class, 'deps']);
+});
         ";
 
         if (!Str::contains(File::get($file), $routeEntry)) {
